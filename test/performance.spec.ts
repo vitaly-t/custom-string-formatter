4f70f8662a885915d92290fa9fd3dd009d276bb6
Vitaly Tomilov <vitaly-t@users.noreply.github.com>
2025-08-05 03:09:36 +0100

    add performance tests

---
+++ test/performance.spec.ts
@@ -0,0 +1,92 @@
+import {createFormatter, IFormatter, IFormattingFilter} from '../src';
+
+class TestFilter implements IFormattingFilter {
+    format(value: any): string {
+        return (value ?? 'null').toString();
+    }
+}
+
+class TestFormatter implements IFormatter {
+    format(value: any): string {
+        return (value ?? 'null').toString();
+    }
+
+    filters = {
+        test: new TestFilter()
+    }
+}
+
+const format = createFormatter(new TestFormatter());
+
+const totalVars = 500_000; // It can do 1 mln, but GitHub will fail
+let output = ''; // expected output
+
+const testData = {
+    // simple property resolution
+    simple: {
+        obj: {},
+        input: ''
+    },
+    // with nested property
+    nested: {
+        obj: {},
+        input: ''
+    },
+    // with a filter
+    filtered: {
+        obj: {},
+        input: ''
+    }
+};
+
+beforeAll(() => {
+    generateData();
+});
+
+function generateData() {
+    for (let i = 0; i < totalVars; i++) {
+        testData.simple.input += `$(prop_${i})`;
+        testData.simple.obj[`prop_${i}`] = i;
+
+        testData.nested.input += `$(prop_${i}.value)`;
+        testData.nested.obj[`prop_${i}`] = {value: i};
+
+        testData.filtered.input += `$(prop_${i}:test)`;
+        testData.filtered.obj[`prop_${i}`] = i;
+
+        output += i;
+    }
+}
+
+describe('performance', () => {
+    describe('for simple resolution', () => {
+        it(`must do at least ${totalVars} replacements per second`, () => {
+            const start = Date.now();
+            const s = format(testData.simple.input, testData.simple.obj);
+            const duration = Date.now() - start;
+            console.log(`duration: ${duration}ms`);
+            expect(s).toEqual(output);
+            expect(duration).toBeLessThan(1000);
+        });
+    });
+    describe('for nested resolution', () => {
+        it(`must do at least ${totalVars} replacements per second`, () => {
+            const start = Date.now();
+            const s = format(testData.nested.input, testData.nested.obj);
+            const duration = Date.now() - start;
+            console.log(`duration: ${duration}ms`);
+            expect(s).toEqual(output);
+            expect(duration).toBeLessThan(1000);
+        });
+    });
+    describe('for filtered resolution', () => {
+        it(`must do at least ${totalVars} replacements per second`, () => {
+            const start = Date.now();
+            const s = format(testData.filtered.input, testData.filtered.obj);
+            const duration = Date.now() - start;
+            console.log(`duration: ${duration}ms`);
+            expect(s).toEqual(output);
+            expect(duration).toBeLessThan(1000);
+        });
+    });
+});
