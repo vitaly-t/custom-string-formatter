4be8a99982b4984dc8e163f50320b43450e44be4
Vitaly Tomilov <vitaly-t@users.noreply.github.com>
2025-08-05 19:08:52 +0100

    adding enumVariables

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,14 @@
 import {IFormatter} from './protocol';
 import {resolveProperty} from './resolver';
 
+const regEx = new RegExp(/\$(?:({)|(\()|(<)|(\[)|(\/))\s*([\w$.]+)\s*(:\s*([\w\$]*)\s*)?\s*(?:(?=\2)(?=\3)(?=\4)(?=\5)}|(?=\1)(?=\3)(?=\4)(?=\5)\)|(?=\1)(?=\2)(?=\4)(?=\5)>|(?=\1)(?=\2)(?=\3)(?=\5)]|(?=\1)(?=\2)(?=\3)(?=\4)\/)/g);
+
 /**
  * Returns a function that formats strings according to the specified configurator.
  */
 export function createFormatter(base: IFormatter) {
-    const reg = new RegExp(/\$(?:({)|(\()|(<)|(\[)|(\/))\s*([\w$.]+)\s*(:\s*([\w\$]*)\s*)?\s*(?:(?=\2)(?=\3)(?=\4)(?=\5)}|(?=\1)(?=\3)(?=\4)(?=\5)\)|(?=\1)(?=\2)(?=\4)(?=\5)>|(?=\1)(?=\2)(?=\3)(?=\5)]|(?=\1)(?=\2)(?=\3)(?=\4)\/)/g);
     return function (text: string, params: { [key: string]: any }) {
-        return text.replace(reg, (...args: string[]) => {
+        return text.replace(regEx, (...args: string[]) => {
             const prop = args[6]; // property name
             const filter = args[8]; // filter, if specified
             const res = resolveProperty(prop, params);
@@ -33,4 +34,46 @@ export function createFormatter(base: IFormatter) {
             return base.format(res.value);
         });
     }
+}
+
+/**
+ * Variable descriptor, as returned from `enumVariables` function.
+ */
+export interface IVariable {
+
+    /**
+     * Exact enumeration match for the variable.
+     */
+    match: string;
+
+    /**
+     * Extracted property name.
+     */
+    property: string;
+
+    /**
+     * Extracted filter name, if specified.
+     */
+    filter?: string;
+}
+
+/**
+ * Enumerates variables from a string, for any kind of reference analysis.
+ *
+ * @param text
+ * Text string that contains variables.
+ *
+ * @returns IVariable[]
+ * An array of matched variables (as descriptors)
+ */
+export function enumVariables(text: string): IVariable[] {
+    return (text.match(regEx) || [])
+        .map(m => {
+            const a = m.match(/\$.\s*([\w$.]+)\s*(:\s*([\w$]+))?/) || [];
+            const result: IVariable = {match: m, property: a[1]};
+            if (a[3]) {
+                result.filter = a[3];
+            }
+            return result;
+        });
 }
