e5b5fff2bc31d7e4f9625c850ee8a40eae421056
Vitaly Tomilov <vitaly-t@users.noreply.github.com>
2025-08-10 17:57:01 +0100

    HTML-decoding is done

--- src/decoder.ts
+++ src/decoder.ts
@@ -1,19 +1,21 @@
-const decodeRegEx = /\\x([\da-f]{1,5})/gi;
+const decodeRegEx = /&#(\d{1,6});|&#x([\da-f]{1,5});/gi;
 
 /**
- * Helper for decoding hex-encoded sequences inside a list of strings.
+ * Helper for decoding HTML-encoded symbols inside a list of strings.
  *
  * @param items
- * List of strings that may have encoded sequences in the form of `\xCODE`,
- * where the `CODE` is 1-5 hex digits (case-insensitive).
+ * List of strings that may contain symbols encoded like this:
+ *  - `&#123;`: decimal symbol code (1-6 digits);
+ *  - `&#x1a3;`: hexadecimal symbol code (1-5 hex digits, case-insensitive);
  *
  * @example
- *  - `\x5e` => `^`
- *  - `\x20ac` => `€`
- *  - `\x1f60a` => `😊`
+ *  - `&#94;` => `^`
+ *  - `&#x20ac;` => `€`
+ *  - `&#x1f60a;` => `😊`
  */
 export function decodeSymbols(items: string[]): string[] {
     return items.map(a => a.replace(decodeRegEx, (...m: string[]) => {
-        return String.fromCodePoint(parseInt(m[1], 16));
+        const code = m[1] ? parseInt(m[1], 10) : parseInt(m[2], 16);
+        return String.fromCodePoint(code);
     }));
 }
--- test/decoder.spec.ts
+++ test/decoder.spec.ts
@@ -2,16 +2,16 @@ import {decodeSymbols} from '../src/decoder';
 
 describe('decodeSymbols', () => {
     it('must handle ASCII symbols', () => {
-        expect(decodeSymbols(['\\x5E', '\\x7B'])).toEqual(['^', '{']);
-        expect(decodeSymbols(['\\x5e', '\\x7b'])).toEqual(['^', '{']);
+        expect(decodeSymbols(['&#94;', '&#123;'])).toEqual(['^', '{']);
+        expect(decodeSymbols(['&#x5E;', '&#x7b;'])).toEqual(['^', '{']);
     });
     it('must handle short Unicode symbols', () => {
         // Symbols: Euro + Sigma + Infinity:
-        expect(decodeSymbols(['\\x20AC', '\\x3A3', '\\x221E'])).toEqual(['€', 'Σ', '∞']);
-        expect(decodeSymbols(['\\x20ac', '\\x3a3', '\\x221e'])).toEqual(['€', 'Σ', '∞']);
+        expect(decodeSymbols(['&#8364;', '&#931;', '&#8734;'])).toEqual(['€', 'Σ', '∞']);
+        expect(decodeSymbols(['&#x20AC;', '&#x3a3;', '&#x221e;'])).toEqual(['€', 'Σ', '∞']);
     });
     it('must handle long Unicode symbols', () => {
-        expect(decodeSymbols(['\\x1F60A', '\\x1F451'])).toEqual(['😊', '👑']);
-        expect(decodeSymbols(['\\x1F60a', '\\x1f451'])).toEqual(['😊', '👑']);
+        expect(decodeSymbols(['&#128522;', '&#128081;'])).toEqual(['😊', '👑']);
+        expect(decodeSymbols(['&#x1F60a;', '&#x1f451;'])).toEqual(['😊', '👑']);
     });
 });
